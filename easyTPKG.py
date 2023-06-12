ba192a37f94fc0db5650c10eb1c7e266ec98e6ec
TrollSkull <leanahuelmiranda@gmail.com>
2023-06-11 19:49:41 -0600

    Update easyTPKG.py

--- easyTPKG.py
+++ easyTPKG.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+import threading
 import time
 import sys
 import os
 
 __author__ = 'TrollSkull'
-__version__= 'v1.1'
+__version__= 'v1.2'
 
 banner = ('''
                  _____ _____ _____ _____ 
@@ -18,45 +19,42 @@ banner = ('''
 def banner_message(color, reset):
     print(color + banner + reset)
 
-def error_message():
-    print('This tool only works on Termux.')
+def error_message(color, reset):
+    print(color + 'This tool only works on Termux.')
     print('\nTo see more about this tool, check:')
-    print('https://github.com/TrollSkull/easyTPKG')
+    print(reset + 'https://github.com/TrollSkull/easyTPKG\n')
 
 if not sys.platform == 'win32':
     if os.system('uname -o') == 'Android':
         os.system('clear')
     else:
-        error_message()
+        error_message('\033[91m', '\033[0m')
 else:
-    error_message()
+    error_message('\033[91m', '\033[0m')
 
 packages = ['coreutils', 'zip', 'unzip', 'unrar', 'nmap', 'wget', 'openssl', 'openssh',
             'vim', 'php', 'python', 'python2', 'python-dev', 'python3', 'java', 'git',
             'dnsutils', 'hydra', 'macchanger', 'curl', 'perl', 'golang']
 
-def install_packages(delay, color, reset):
-    for package in packages:
-
-        print(color + f'\nInstalling {package}...\n' + reset)
-        os.system(f'pkg install {package} -y &> /dev/null')
-
-        time.sleep(delay)
-
+def install_packages(package, color, reset):
+    print(color + f'\nInstalling {package}...\n' + reset)
+    os.system(f'pkg install {package} -y &> /dev/null')
+        
 if __name__ == '__main__':
     try:
+        os.system('clear')
         banner_message('\033[92m', '\033[0m')
 
-        print('\n[easyTPKG] Updating and upgrading packages.\n')
-        os.system ('apt update && apt upgrade -y')
+        print('\033[92m' + '\n[easyTPKG] ' + '\033[0m' + 'Updating and upgrading packages.\n')
+        os.system ('apt update && apt upgrade -y &> /dev/null')
 
         if not os.path.exists('/data/data/com.termux/files/home/storage'):
-            print('\n[easyTPKG] We need storage permission.\n')
+            print('\033[92m' + '\n[easyTPKG] ' + '\033[0m' + 'We need storage permission.\n')
             os.system ('termux-setup-storage')
 
-        install_packages(delay = 1,
-                         reset = '\033[0m',
-                         color = '\033[92m')
+        for package in packages:
+            threading.Thread(target = install_packages, args = (package, '\033[0m', '\033[92m')).start()
+            time.sleep(5)
 
     except KeyboardInterrupt:
         sys.exit('[CTRL+C] Detected, exiting.')
